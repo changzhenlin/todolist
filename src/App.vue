d3a14e103af86501fa7b255365150b4378e16a66
changzhenlin <chang_zhenlin@126.com>
2022-12-27 18:14:44 +0800

    input 需要加 handleinput

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,31 @@
 <script setup>
+import {ref} from 'vue'
+    const newjob = ref('')
+    const itemList = ref([])
+    const addJob = () => {
+      itemList.value.push({
+        name: newjob.value,
+        undo: true,
+        id: Math.random()
+      })
+      newjob.value = ''
+    }
+    const handleInput = (e) => {
+      newjob.value = e.target.value
+    }
 </script>
 
 <template>
   <header class="bg-pink-200">
     <div class="grid m-10 p-2 grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        1
-      </div>
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        2
-      </div>
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        3
-      </div>
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        4
-      </div>
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        6
-      </div>
+      <input type="text" :value="newjob" @input="handleInput" />
+      <button @click="addJob">添加</button>
     </div>
   </header>
 
   <main class="bg-gray-200">
-    <div class="grid m-10 p-2 grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        1
-      </div>
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        2
-      </div>
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        3
-      </div>
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        4
-      </div>
-      <div class= "h-20 rounded-2xl flex flex-col justify-center items-center bg-slate-50">
-        6
-      </div>
+    <div v-for="item in itemList" class="grid m-10 p-2 grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
+      {{ item.name }}
     </div>
   </main>
 </template>
