7317397983cacaef49e73a0fc2d67e1c7e22e361
changzhenlin <chang_zhenlin@126.com>
2022-12-30 18:30:11 +0800

    some code

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,21 @@ import {ref} from 'vue'
 </script>
 
 <template>
-  <header class="bg-pink-200">
-    <div class="grid m-10 p-2 grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
-      <input type="text" :value="newjob" @input="handleInput" />
-      <button @click="addJob">添加</button>
+  <header class="bg-gray-600 flex flex-col items-start sm:items-center h-56 p-8 rounded-b-2xl">
+    <h1 class="text-2xl text-white">Management <span class="text-blue-400">Task</span></h1>
+    <p class="text-white text-md">check your task every day and checked</p>
+    <div class="mt-10 overflow-hidden">
+      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap">Todo</button>
+      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap">Completed</button>
+      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap">All task</button>
     </div>
   </header>
 
   <main class="bg-gray-200">
+    <div class="grid m-10 p-2 grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
+      <input type="text" :value="newjob" @input="handleInput" />
+      <button @click="addJob">添加</button>
+    </div>
     <div v-for="item in itemList" class="grid m-10 p-2 grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
       {{ item.name }}
     </div>
