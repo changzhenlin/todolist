c99eb67677482a8e7cfb0430f2efa5e5cac8f87d
changzhenlin <chang_zhenlin@126.com>
2023-01-09 18:40:42 +0800

    基本功能实现

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <script setup>
-import {ref} from 'vue'
+    import {ref,watch} from 'vue'
+    let date = localStorage.getItem('tododate')
+    let today = new Date().toISOString().substring(0, 10);
+    let itemListAll = ref([])
+    let allTask = ref([])
+    if (!date || date !== today) {
+      localStorage.setItem('tododate', today)
+    } else {
+      if (localStorage.getItem('itemList')) {
+        itemListAll.value = JSON.parse(localStorage.getItem('itemList'))
+        allTask.value = itemListAll.value
+      }
+    }
     const newjob = ref('')
-    const itemList = ref([])
     const addJob = () => {
-      itemList.value.push({
+      let obj = {
         name: newjob.value,
         undo: true,
         id: Math.random()
-      })
+      }
+      itemListAll.value.push(obj)
+      allTask.value = itemListAll
+      localStorage.setItem('itemList', JSON.stringify(itemListAll.value))
       newjob.value = ''
     }
     const handleInput = (e) => {
       newjob.value = e.target.value
     }
+    const viewTodo = () => {
+      itemListAll.value = allTask.value.filter(item => item.undo === true)
+    }
+    const viewAll = () => {
+      itemListAll.value = allTask.value
+    }
+    const viewCompleted = () => {
+      itemListAll.value = allTask.value.filter(item => item.undo === false)
+    }
+    // itemList = itemListAll
 </script>
 
 <template>
@@ -20,19 +44,20 @@ import {ref} from 'vue'
     <h1 class="text-2xl text-white">Management <span class="text-blue-400">Task</span></h1>
     <p class="text-white text-md">check your task every day and checked</p>
     <div class="mt-10 overflow-hidden">
-      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap">Todo</button>
-      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap">Completed</button>
-      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap">All task</button>
+      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap" @click="viewTodo">Todo</button>
+      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap" @click="viewCompleted">Completed</button>
+      <button class="bg-blue-400 text-white rounded-lg px-4 py-2 m-2 whitespace-nowrap" @click="viewAll">All task</button>
     </div>
   </header>
 
   <main class="bg-gray-200">
-    <div class="grid m-10 p-2 grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
-      <input type="text" :value="newjob" @input="handleInput" />
-      <button @click="addJob">添加</button>
-    </div>
-    <div v-for="item in itemList" class="grid m-10 p-2 grid-flow-col grid-rows-2 grid-cols-3 gap-4 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
+    <div v-for="item in itemListAll" class="grid m-10 p-4 grid-flow-col text-3xl
+    hover:cursor-pointer hover:from-blue-200 hover:to-rose-300 grid-rows-1 grid-cols-2 gap-2 bg-gradient-to-br rounded-2xl from-pink-300 to-rose-300">
       {{ item.name }}
+    </div>
+    <div class="grid m-10 p-2 grid-flow-row  grid-rows-1 grid-cols-2 gap-1 bg-gradient-to-br rounded-xl from-pink-300 to-rose-300">
+      <input type="text" class="p-1" :value="newjob" @input="handleInput" />
+      <button class="inline-block w-20 bg-slate-600 h-10 rounded-sm text-white" @click="addJob">添加</button>
     </div>
   </main>
 </template>
